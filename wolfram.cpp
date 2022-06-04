3543508f860978f9533e9a2bc8f67fd11815b114
Alexis Olvera <alexis.ol.me@gmail.com>
2022-06-04 11:41:34 -0500

    Con historial

--- wolfram.cpp
+++ wolfram.cpp
@@ -7,7 +7,7 @@
 
 #define ANCHO_TABLERO 1000
 #define ALTO_VENTANA 700
-#define TAM 500
+#define TAM 10000
 
 std::mutex mu;
 
@@ -30,18 +30,18 @@ void avanzar(std::bitset<N> &fila, unsigned char &configuracion, bool &play, int
         }
         historial.push_back(fila);
         mu.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
     }
 }
 
 int main(){
-    unsigned char configuracion = 186;
+    unsigned char configuracion = 30;
     std::bitset<TAM> fila;
     std::vector<std::bitset<TAM>> historial;
-    fila[TAM/2]=1;
+    fila[1]=1;
     sf::RenderWindow window(sf::VideoMode(ANCHO_TABLERO, ALTO_VENTANA), "AC 1 dimension");
-    const int tam_celula = ANCHO_TABLERO/TAM;
+    const int tam_celula = 5;//ANCHO_TABLERO/TAM;
     const sf::Vector2f tam_vector(tam_celula, tam_celula);
     const float ancho_total = ANCHO_TABLERO;
 
@@ -113,14 +113,23 @@ int main(){
         }
         window.clear();
         window.setView(view_tablero);
+        mu.lock();
+        sf::RectangleShape muertas;
+        muertas.setPosition(0, 0);
+        muertas.setSize(sf::Vector2f(ALTO_VENTANA*tam_celula, ANCHO_TABLERO*tam_celula));
+        muertas.setFillColor(c_muertas);
+        window.draw(muertas);
         for(int y = 0; y<historial.size(); y++)
         for(int x = 0; x<TAM; x++){
-            sf::RectangleShape celula;
-            celula.setPosition(x * tam_celula, y * tam_celula);
-            celula.setSize(tam_vector);
-            celula.setFillColor(historial[y][x]?c_vivas:c_muertas);
-            window.draw(celula);
+            if(historial[y][x]){ //vivas
+                sf::RectangleShape celula;
+                celula.setPosition(x * tam_celula, y * tam_celula);
+                celula.setSize(tam_vector);
+                celula.setFillColor(c_vivas);
+                window.draw(celula);
+            }
         }
+        mu.unlock();
         window.display();
         sf::sleep(sf::milliseconds(100));
     }
